f4bdb405d8c6a3445d919eef66f8011ab99ee081
LIYOU ZHOU <leothemagnificent@gmail.com>
2017-12-31 02:45:17 +0000

    Using generator to save memory consumption
    from 5gb to 500 mb

--- train.py
+++ train.py
@@ -8,11 +8,37 @@ from keras.layers import Flatten, Dense, Lambda, Activation, Dropout, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+def data_generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                img = cv2.imread(batch_sample[0])
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                if batch_sample[1]:
+                    img = np.fliplr(img)
+
+                angle = float(batch_sample[2])
+
+                images.append(img)
+                angles.append(angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield shuffle(X_train, y_train)
 
 def main(data_paths, output_fn, correction, nb_epoch, base_model):
     # prepare data
-    images = []
-    measurements = []
+    samples = []
     for data_path in data_paths:
         data_csv_path = path.join(data_path, "driving_log.csv")
         with open(data_csv_path) as csvfile:
@@ -22,25 +48,22 @@ def main(data_paths, output_fn, correction, nb_epoch, base_model):
                     img_fn = path.basename(img_fn)
                     img_fn = path.join(data_path, "IMG", img_fn)
 
-                    img = cv2.imread(img_fn)
-                    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                    images.append(img)
-                    images.append(np.fliplr(img))
-
                     measurement = float(line[3])
                     if   ('left' in img_fn): measurement += correction
                     elif ('right' in img_fn): measurement -= correction
-                    measurements.append(measurement)
-                    measurements.append(-measurement)
 
-    X_train = np.array(images)
-    y_train = np.array(measurements)
+                    # path to image, flipped?, steering angle
+                    samples.append((img_fn, False, measurement))
+                    samples.append((img_fn, True, -measurement))
 
-    print(X_train.shape)
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+    train_generator = data_generator(train_samples, batch_size=32)
+    validation_generator = data_generator(validation_samples, batch_size=32)
 
     # define model
     model = Sequential()
-    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=X_train.shape[1:]))
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
     model.add(Lambda(lambda x: x/255.0 - 0.5))
     model.add(Convolution2D(24, 5, 5, activation="relu"))
     model.add(MaxPooling2D((2, 2)))
@@ -64,7 +87,11 @@ def main(data_paths, output_fn, correction, nb_epoch, base_model):
     if base_model:
         model.load_weights(base_model)
 
-    history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=nb_epoch)
+    history_object = model.fit_generator(train_generator,
+                                         samples_per_epoch=len(train_samples),
+                                         validation_data=validation_generator,
+                                         nb_val_samples=len(validation_samples),
+                                         nb_epoch=nb_epoch)
 
     # save output
     model.save(output_fn)
